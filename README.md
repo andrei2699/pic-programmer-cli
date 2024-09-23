87cdab09c40994641f62e4aebb17a5b65bdb2612
Andrei Timar <atimar999@gmail.com>
2024-09-23 09:03:36 +0300

    feat: implement send all instructions from file

--- README.md
+++ README.md
@@ -8,8 +8,7 @@ A simple cli app that connects to an Arduino and send the contents of a hex file
 - read lines from input file and send them one by one until end of file
 - read message from programmer
 
-## Send Package
+Each line of the hex file is sent until the end of file or `end of file instruction` (:00000001FF).
 
-| Header | a |
-|--------|---|
-| Data   | a |
+After each line, the programmer will send either `Y` if the instruction was read successfully with the checksum
+verification or `R` if the last instruction needs to be resent
--- src/programmer/serial_programmer.rs
+++ src/programmer/serial_programmer.rs
@@ -7,6 +7,8 @@ use std::str;
 
 const READY_MESSAGE: &'static str = "Programmer ready!";
 const END_OF_FILE: &'static str = ":00000001FF";
+const OK_INSTRUCTION: &'static str = "Y";
+const RESEND_INSTRUCTION: &'static str = "R";
 
 pub struct SerialProgrammer<R: ReadSerial, W: WriteSerial> {
     reader: R,
@@ -19,24 +21,9 @@ impl<R: ReadSerial, W: WriteSerial> SerialProgrammer<R, W> {
     }
 
     pub fn program(&mut self, port: &mut Box<dyn SerialPort>, lines: Lines<BufReader<File>>) {
-        let mut received_data = String::new();
-
         self.wait_for_programmer_to_be_ready(port);
-        received_data.clear();
 
-        for line in lines {
-            let string = line.unwrap();
-            let trimmed_line = string.trim();
-            if trimmed_line.is_empty() {
-                continue;
-            }
-
-            self.writer.write(port, trimmed_line);
-
-            if trimmed_line.contains(END_OF_FILE) {
-                break;
-            }
-        }
+        self.send_lines(port, lines);
 
         println!("finished programming!")
     }
@@ -52,12 +39,39 @@ impl<R: ReadSerial, W: WriteSerial> SerialProgrammer<R, W> {
             }
         }
     }
+
+    fn send_lines(&mut self, port: &mut Box<dyn SerialPort>, lines: Lines<BufReader<File>>) {
+        let mut received_data = String::new();
+
+        'lines: for line in lines {
+            let string = line.unwrap();
+            let trimmed_line = string.trim();
+            if trimmed_line.is_empty() {
+                continue;
+            }
+
+            received_data.clear();
+            while !received_data.contains(OK_INSTRUCTION) {
+                self.writer.write(port, trimmed_line);
+
+                if trimmed_line.contains(END_OF_FILE) {
+                    break 'lines;
+                }
+
+                self.reader.read(port, &mut received_data);
+                if received_data.contains(RESEND_INSTRUCTION) {
+                    println!("resending instruction {}", trimmed_line);
+                    received_data.clear();
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::programmer::file_reader::get_lines;
-    use crate::programmer::serial_programmer::{SerialProgrammer, END_OF_FILE};
+    use crate::programmer::serial_programmer::{SerialProgrammer, END_OF_FILE, OK_INSTRUCTION, RESEND_INSTRUCTION};
     use crate::programmer::serial_reader::ReadSerial;
     use crate::programmer::serial_writer::WriteSerial;
     use crate::programmer::test_serial_port::TestSerialPort;
@@ -71,7 +85,6 @@ mod test {
         let writer = WriterTest { data: vec![] };
         let mut port: Box<dyn SerialPort> = Box::new(TestSerialPort {});
         let lines = get_lines(&get_full_path("test-files/empty-file.hex").to_string_lossy().to_string());
-
         let mut programmer = SerialProgrammer::new(reader, writer);
 
         programmer.program(&mut port, lines);
@@ -85,7 +98,6 @@ mod test {
         let writer = WriterTest { data: vec![] };
         let mut port: Box<dyn SerialPort> = Box::new(TestSerialPort {});
         let lines = get_lines(&get_full_path("test-files/empty-file.hex").to_string_lossy().to_string());
-
         let mut programmer = SerialProgrammer::new(reader, writer);
 
         programmer.program(&mut port, lines);
@@ -99,7 +111,6 @@ mod test {
         let writer = WriterTest { data: vec![] };
         let mut port: Box<dyn SerialPort> = Box::new(TestSerialPort {});
         let lines = get_lines(&get_full_path("test-files/empty-file.hex").to_string_lossy().to_string());
-
         let mut programmer = SerialProgrammer::new(reader, writer);
 
         programmer.program(&mut port, lines);
@@ -107,14 +118,12 @@ mod test {
         assert_eq!(programmer.writer.data.is_empty(), true);
     }
 
-
     #[test]
     fn given_file_with_only_eof_should_write_one_instruction() {
         let reader = ReaderTest { data: vec![String::from("Programmer ready!")], index: 0 };
         let writer = WriterTest { data: vec![] };
         let mut port: Box<dyn SerialPort> = Box::new(TestSerialPort {});
         let lines = get_lines(&get_full_path("test-files/only-eof.hex").to_string_lossy().to_string());
-
         let mut programmer = SerialProgrammer::new(reader, writer);
 
         programmer.program(&mut port, lines);
@@ -128,7 +137,6 @@ mod test {
         let writer = WriterTest { data: vec![] };
         let mut port: Box<dyn SerialPort> = Box::new(TestSerialPort {});
         let lines = get_lines(&get_full_path("test-files/eof-at-the-beginning.hex").to_string_lossy().to_string());
-
         let mut programmer = SerialProgrammer::new(reader, writer);
 
         programmer.program(&mut port, lines);
@@ -136,10 +144,57 @@ mod test {
         assert_eq!(programmer.writer.data[0], END_OF_FILE);
     }
 
+    #[test]
+    fn given_file_with_all_instructions_sent_correctly_should_write_all_instructions_once() {
+        let reader = ReaderTest {
+            data: vec![
+                String::from("Programmer ready!"),
+                String::from(OK_INSTRUCTION),
+                String::from(OK_INSTRUCTION)
+            ],
+            index: 0,
+        };
+        let writer = WriterTest { data: vec![] };
+        let mut port: Box<dyn SerialPort> = Box::new(TestSerialPort {});
+        let lines = get_lines(&get_full_path("test-files/multiple-instructions.hex").to_string_lossy().to_string());
+        let mut programmer = SerialProgrammer::new(reader, writer);
+
+        programmer.program(&mut port, lines);
+
+        assert_eq!(programmer.writer.data[0], ":02002400BE1C");
+        assert_eq!(programmer.writer.data[1], ":02002500BE1B");
+        assert_eq!(programmer.writer.data[2], END_OF_FILE);
+    }
+
+    #[test]
+    fn given_file_with_one_instruction_sent_with_problems_should_retry_the_write_of_that_instruction() {
+        let reader = ReaderTest {
+            data: vec![
+                String::from("Programmer ready!"),
+                String::from(RESEND_INSTRUCTION),
+                String::from(OK_INSTRUCTION),
+                String::from(OK_INSTRUCTION)
+            ],
+            index: 0,
+        };
+        let writer = WriterTest { data: vec![] };
+        let mut port: Box<dyn SerialPort> = Box::new(TestSerialPort {});
+        let lines = get_lines(&get_full_path("test-files/multiple-instructions.hex").to_string_lossy().to_string());
+        let mut programmer = SerialProgrammer::new(reader, writer);
+
+        programmer.program(&mut port, lines);
+
+        assert_eq!(programmer.writer.data[0], ":02002400BE1C");
+        assert_eq!(programmer.writer.data[1], ":02002400BE1C");
+        assert_eq!(programmer.writer.data[2], ":02002500BE1B");
+        assert_eq!(programmer.writer.data[3], END_OF_FILE);
+    }
+
     struct ReaderTest {
         data: Vec<String>,
         index: usize,
     }
+
     impl ReadSerial for ReaderTest {
         fn read(&mut self, _port: &mut Box<dyn SerialPort>, received_data: &mut String) {
             let string = self.data[self.index].as_str();
@@ -151,6 +206,7 @@ mod test {
     struct WriterTest {
         data: Vec<String>,
     }
+
     impl WriteSerial for WriterTest {
         fn write(&mut self, _port: &mut Box<dyn SerialPort>, string: &str) {
             self.data.push(string.to_string())
